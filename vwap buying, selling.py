cb91d81e281fcea170b1f4c0915d5eebfcd7d0eb
charan9666 <charansai.scs@gmail.com>
2022-02-28 11:34:07 +0530

    Update vwap buying, selling.py

--- vwap buying, selling.py
+++ vwap buying, selling.py
@@ -1,10 +1,7 @@
-import re
-from numpy import append, true_divide
+from datetime import datetime
 from pynse import *
-import requests
-from pprint import pprint
 import time
-import ORB
+
 
 watchlist = ['SBIN','INFY','WIPRO','RELIANCE','ADANIENT',
                 'TATAPOWER', 'TECHM', 'TITAN','JSWSTEEL', 'SBILIFE',  'M&M',
@@ -13,26 +10,22 @@ nse=Nse()
 count  = 0
 buying = []
 selling = []
-
-for i in watchlist:
-  #  print(i)
-    while  count <= 74:
+while True:
+  for i in watchlist:
+    #  print(i)
             a = nse.get_quote(i)
             vwap = int(a['vwap'])
             ltp = int(a['lastPrice'])
-           # print("ltp is:",ltp)
-           # print("vwap is:",vwap)
+            # print("ltp is:",ltp)
+            # print("vwap is:",vwap)
             if vwap < ltp:
-                buying.append(str(i))
-                #print("BUY SIGNEL in: "+str(i))
+                  buying.append(str(i))
+                  print("BUY SIGNEL in: "+str(i))
             elif vwap>ltp:
-                selling.append(str(i))
-               # print("SELL SIGNEL: "+str(i))
-            #prnt(i)
+                  selling.append(str(i))
+                  print("SELL SIGNEL: "+str(i))
+              #prnt(i)
             count += 1
-            break
-    #time.sleep(2)
-print("your buying stocks is: ",buying)
-print("your selling stocks is: ",selling)    
-time.sleep(300)         
-
+  print("-----------------------------------------------------")            
+  time.sleep(300)
+  
